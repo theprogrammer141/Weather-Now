f343569d1a245583234015784f4aeec8c6c73d16
theprogrammer141 <abdullah.k9913@gmail.com>
2026-04-18 15:24:05 +0500

    Rendered Hourly forecast data

--- js/script.js
+++ js/script.js
@@ -14,7 +14,21 @@ const currentWeatherIcon = document.querySelector('.current-weather__icon');
 const currentWeatherTemperature = document.querySelector(".current-weather__temp");
 const dailyForecastGrid = document.querySelector(".daily-forecast-cards-grid");
 const hourlyForecastColumn = document.querySelector(".hourly-forecast-column");
+const daysDropDown = document.querySelector('#days-select .custom-select__options');
+const daysDropDownValue = document.querySelector("#days-select .custom-select__value");
+const unitsDropDown = document.querySelector('#units-select .custom-select__options');
+const switchToImperial = document.getElementById('switch-to-imperial');
+const switchToCelsius = document.getElementById('switch-to-celsius');
+const switchToFahrenheit = document.getElementById('switch-to-fahrenheit');
+const switchToKmh = document.getElementById('switch-to-kmh');
+const switchToMph = document.getElementById('switch-to-mph');
+const switchToMillimeters = document.getElementById('switch-to-mm');
+const switchToInches = document.getElementById('switch-to-in');
 
+//* Variables
+let currentWeatherData = null;
+
+//* Event Listeners
 customSelectTrigger.forEach(trigger => {
     trigger.addEventListener('click', function(e){
         const parent = this.closest('.custom-select');
@@ -44,6 +58,12 @@ searchBtn.addEventListener('click', function(e){
     fetchWeather(city);
 });
 
+// unitsDropDown.addEventListener('click', function(e){
+//     if(e.target === switchToImperial){
+//         switchToImperial.innerHTML = 'Switch to Metric';
+//     }
+// })
+//* Functions
 const fetchWeather = async function(cityName){
     try{
         const geoResponse = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${cityName}&count=10&language=en&format=json`);
@@ -59,9 +79,10 @@ const fetchWeather = async function(cityName){
         const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,wind_speed_10m,weather_code&hourly=temperature_2m,weather_code&daily=weather_code,temperature_2m_max,temperature_2m_min&timezone=auto&forecast_days=7`);
     
         const weatherData = await weatherResponse.json();
+        currentWeatherData = weatherData;
     
-        renderWeather(geoData, weatherData);
-        console.log(weatherData);
+        renderWeather(geoData, currentWeatherData);
+        console.log(currentWeatherData);
         console.log(geoData);
     }catch(error){
         console.log(error);
@@ -83,9 +104,9 @@ const renderWeather = function(geoData, weatherData){
 
     currentWeatherIcon.src = getWeather(weatherData.current.weather_code);
 
-    currentWeatherTemperature.textContent = weatherData.current.apparent_temperature + weatherData.current_units.apparent_temperature;
+    currentWeatherTemperature.textContent = Math.round(weatherData.current.apparent_temperature) + weatherData.current_units.apparent_temperature;
 
-    feelsLike.textContent = weatherData.current.temperature_2m + weatherData.current_units.temperature_2m;
+    feelsLike.textContent = Math.round(weatherData.current.temperature_2m) + weatherData.current_units.temperature_2m;
     
     humidity.textContent = weatherData.current.relative_humidity_2m + weatherData.current_units.relative_humidity_2m;
 
@@ -109,6 +130,63 @@ const renderWeather = function(geoData, weatherData){
                                 </div>`;
 
         dailyForecastGrid.insertAdjacentHTML('beforeend', dailyForecastCard);
+
+        const daysDropDownOption = `<li class="custom-select__option" role="option" data-index=${i}>${dayName}</li>`;
+
+        daysDropDown.insertAdjacentHTML('beforeend', daysDropDownOption);
+    }
+
+    renderHourlyForecast(weatherData, 0);
+
+    const todayName = new Date(weatherData.daily.time[0]).toLocaleDateString('en-US', {weekday: 'long'});
+
+    daysDropDownValue.textContent = todayName;
+
+    daysDropDown.querySelectorAll('.custom-select__option').forEach(option => {
+        option.addEventListener('click', function(){
+
+            daysDropDown.querySelectorAll('.custom-select__option').forEach(opt =>{
+                opt.classList.remove("selected");
+            });
+
+            const dayIndex = parseInt(this.dataset.index);
+            
+            daysDropDownValue.textContent = this.textContent;
+            
+            this.closest('.custom-select').classList.remove('open');
+
+            this.classList.add('selected');
+
+            renderHourlyForecast(currentWeatherData, dayIndex);
+        });
+    })
+
+}
+
+const renderHourlyForecast = function(weatherData, dayIndex){
+    hourlyForecastColumn.innerHTML = '';
+
+    const startIndex = dayIndex * 24;
+    const endIndex = startIndex + 24;
+
+    for(let i = startIndex; i < endIndex; i++){
+        const weatherIcon = getWeather(weatherData.hourly.weather_code[i]);
+
+        const currentHour = new Date(weatherData.hourly.time[i]).toLocaleTimeString('en-US', {
+            hour: 'numeric',
+            hour12: true
+        });
+        
+        const currentTemperature = Math.round(weatherData.hourly.temperature_2m[i]) + "°";
+
+        const hourlyForecastCard = `<div class="card">
+                                    <img src="${weatherIcon}" alt=""
+                                    aria-label="Hourly-weather-Icon" class="hourly-weather-icon"/>
+                                    <p class="current-hour" id="current-hour" aria-label="Current-hour">${currentHour}</p>
+                                    <p class="current-hour-temperature" id="current-hour-temperature" aria-label="Current-Hour-Temperature">${currentTemperature}</p>
+                                    </div>`;
+
+        hourlyForecastColumn.insertAdjacentHTML('beforeend', hourlyForecastCard);
     }
 }
 
